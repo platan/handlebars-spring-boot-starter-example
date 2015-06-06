c89a4a335f3a1e7f6c877a18ca8f4466c39f86ed
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2015-06-06 21:41:35 +0200

    Using a simple property in a template

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,15 @@
 package hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("name", "World");
         return "hello";
     }
 
